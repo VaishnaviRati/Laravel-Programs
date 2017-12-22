679c43b4e887d3c7b1c8bd0bd3723cf80588ddb3
Vaishnavi R <you@example.com>
2017-12-22 09:54:54 +0530

    database n some example

---
+++ Laravel/resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+</head>
+<body>
+<center><h1 style="color: green">Here is your details</h1></center>
+
+<h2>Hello <?= $name; ?></h2>
+</body>
+</html>
+
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+   // return view('about',[ 'name'=> 'World']);
+	//return view('about')->with('name','Vaishnavi');
+	$name = 'World';
+	//return view('about',['name'=>$name]);
+	return view('about',compact('name'));
+});
+
+Route::get('/info', function () {
+
+	$tasks = DB::table('tasks')->latest()->get();
+
+	return view('Information',compact('tasks'));
+});
+
+
+Route::get('/info/task/{task}', function ($id) {
+
+
+	$tasks = DB::table('tasks')->find($id);
+	dd($tasks);
+
+	return view('show',compact('tasks'));
+});
+
+
+
